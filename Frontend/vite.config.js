2241d1924fce5c06c63f6b63811e8f6663bcc4ac
piragenth <118745126+piragenth@users.noreply.github.com>
2024-12-04 21:46:27 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kajepana-art/",
+  base: "/kajepana-art/Frontend",
 });
