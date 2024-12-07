17874c3fee533989fb158a33bf645f4f74689fce
piragenth-ltfb <111213108+piragenth-ltfb@users.noreply.github.com>
2024-12-07 22:49:20 +0530

    Update Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,8 +1,32 @@
 import Logo from "../assets/drawing-pencil-pencil-with-word-pen-it_339976-59924.avif";
+import { NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-      <img src={Logo} alt="" />
+      <img src={Logo} className="w-36" alt="" />
+
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+        <NavLink to="/" className="flex flex-col items-center gap-1">
+          <p>Home</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
+        </NavLink>
+        <NavLink to="/Work" className="flex flex-col items-center gap-1">
+          <p>Work</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
+        </NavLink>
+        <NavLink to="/Prints" className="flex flex-col items-center gap-1">
+          <p>Prints</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
+        </NavLink>
+        <NavLink to="/Contact" className="flex flex-col items-center gap-1">
+          <p>Contact</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
+        </NavLink>
+        <NavLink to="/About" className="flex flex-col items-center gap-1">
+          <p>About</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
+        </NavLink>
+      </ul>
     </div>
   );
 }
